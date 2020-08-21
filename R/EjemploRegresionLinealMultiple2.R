90cefa2e4ea29226eabc92a8837c22fa46dbacea
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-08-21 19:59:24 -0300

    Con problemas

--- R/EjemploRegresionLinealMultiple2.R
+++ R/EjemploRegresionLinealMultiple2.R
@@ -2,6 +2,7 @@
 
 library(readxl)
 library(dplyr)
+library(ggcorrplot) #ggcorrplot
 
 # --------------------------------------Datos------------------------------------
 
@@ -17,4 +18,23 @@ datos <- datos_crudos %>%
 
 # La idea es practicar el uso de variables categoricas asi que en principio no
 # vamos a usar todas las variables
+
+# Empezamos analizando si existe relación lineal entre nuestras variables
+
+datos %>% 
+  select(carlength, carwidth, carheight, horsepower, wheelbase, curbweight, enginesize, 
+         boreratio, stroke, compressionratio, peakrpm, citympg, highwaympg, price) %>% 
+  cor(method = "pearson") %>% 
+  ggcorrplot(lab = TRUE)
+
+ggplot(datos, aes(fueltype, price, fill = fueltype)) +
+  geom_boxplot() +
+  geom_jitter(width = .1)
+
+# Genearmos el modelo con las variables anteriores
+
+modelo <- lm(price ~ carlength + carwidth + carheight + horsepower, wheelbase + curbweight + 
+     enginesize + boreratio + stroke + compressionratio + peakrpm + citympg + 
+     highwaympg + fueltype, data = datos)
+
 
