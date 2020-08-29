dfcf8612686b0e0f8e693cec6f976b97947ddc82
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-08-28 21:43:39 -0300

    asdas

--- R/RegresionLogisticaMultiple.R
+++ R/RegresionLogisticaMultiple.R
@@ -11,7 +11,7 @@ datos_crudos <- read.csv(file ="./data/train.csv")
 
 datos_crudos$PassengerId <- NULL
 datos_crudos$Name <- NULL
-datos <- datos_crudos %>% 
+datos_it0 <- datos_crudos %>% 
   rename(sobrevivio = Survived, clase = Pclass, sexo = Sex, edad = Age, her_esp = SibSp,
          padre_hijo = Parch, ticket = Ticket, precio_ticket = Fare, nro_cabina = Cabin,
          puerto_embarcacion = Embarked)
@@ -19,17 +19,19 @@ datos <- datos_crudos %>%
 
 # --------------------------------------Exploración datos------------------------------------
 
-write.csv(summary(datos), file = "./example.csv")
+write.csv(summary(datos_it0), file = "./resultados/summary_datos_it0.csv")
 
-# Sacamos los NAs en edad aproximando con su media
+# Sacamos los NAs en edad aproximando con su media y convertimos a factor las variables
+# necesarias
 
-media_edad <- mean(datos$edad, na.rm = TRUE)
-lista_indices <- which(is.na(datos$edad))
-datos$edad <- replace(datos$edad, lista_indices, media_edad)
+media_edad <- mean(datos_it0$edad, na.rm = TRUE)
+lista_indices <- which(is.na(datos_it0$edad))
+datos_it0$edad <- replace(datos_it0$edad, lista_indices, media_edad)
 
-# Convertimos a factor las variables necesarias
 
-datos <- datos %>% 
-  mutate(sobrevivio = as.factor(sobrevivio), clase = as.factor(clase))
-
-write.csv(summary(datos), file = "./example.csv")
+datos_it1 <- datos_it0 %>% 
+  mutate(sobrevivio = as.factor(sobrevivio), clase = as.factor(clase), sexo = as.factor(sexo),
+         nro_cabina = as.factor(nro_cabina), ticket = as.factor(ticket),
+         puerto_embarcacion = as.factor(puerto_embarcacion)) 
+  
+write.csv(summary(datos_it1), file = "./resultados/summary_datos_it1.csv")
